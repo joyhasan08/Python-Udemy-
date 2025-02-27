3a58c1f8b6e584124bb09f315fe9fe0e91ea5a4a
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2025-02-27 23:06:35 +0600

    python variable

---
+++ Basic Python/PythonVariables.py
@@ -0,0 +1,25 @@
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+# Variables do not need to be declared with any particular type and can even change type after they have been set.
+
+
+# Casting
+# If you want to specify the data type of a variable, this can be done with casting.
+# Example
+a = str(3)    # x will be '3'
+b = int(3)    # y will be 3
+c = float(3)  # z will be 3.0
+print(a)
+print(b)
+print(c)
+
+
+# Get the Type
+# You can get the data type of a variable with the type() function.
+# Example
+age = 5
+name = "John"
+print(type(age))
+print(type(name))
