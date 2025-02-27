a11d8cbf88a30db89799c74481990fc7ea4592b0
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2025-02-27 22:35:59 +0600

    input output

---
+++ Basic Python/inputAndPrint.py
@@ -0,0 +1,9 @@
+print('Whats your name?')
+name = input() # The input() function is used to get input from the user.
+print('Hello' + name + 'lets learn python')
+print('How old are you?')
+age = input()
+print("You Will be " + str(int(age) + 1) + " in a year.")
+print('you will be '+ str(int(age)+5) +' in 2030')
+# The input() function returns a string value. If you want to use it as an integer, you need to convert it using the int() function.
+
---
+++ hello.py
@@ -0,0 +1,1 @@
+print ('hello joy . lets learn python')
